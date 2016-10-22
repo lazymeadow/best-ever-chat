b179ed03ad6a946519034e538ec39e10aa150228
audrey <audreymccormick@outlook.com>
2016-10-22 10:44:02 -0600

    adding all the files

---
+++ app.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import time
+from collections import deque
+
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, disconnect
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = "eventlet"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bananaPUDDINGfudgesicleFACE'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+
+users = {}
+
+history = deque(maxlen=100)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('connect_message', namespace='/chat')
+def connect_message(message):
+    session['user'] = message['user']
+    if session['user'] not in users:
+        users[session['user']] = {'color': message['color']}
+    new_msg = {'user': 'Server', 'data': message['user'] + ' has connected!', 'time': time.time()}
+    emit('chat_response', new_msg, broadcast=True)
+    emit('user_list', {'data': users.keys()}, broadcast=True)
+
+
+@socketio.on('broadcast_message', namespace='/chat')
+def broadcast_message(message):
+    new_msg = {'user': message['user'], 'data': message['data'], 'time': time.time(),
+               'color': users[message['user']]['color']}
+    history.append(new_msg)
+    emit('chat_response', new_msg, broadcast=True)
+
+
+@socketio.on('disconnect_request', namespace='/chat')
+def disconnect_request():
+    disconnect(None)
+
+
+@socketio.on('update_user', namespace='/chat')
+def update_user(data):
+    if 'newColor' in data['data'].keys():
+        users[data['user']]['color'] = data['data']['newColor']
+        emit('update_response', {'user': 'Server', 'data': 'Color updated', 'time': time.time()})
+
+    if 'newUser' in data['data'].keys():
+        if data['data']['newUser'] in users.keys():
+            emit('update_response',
+                 {'user': 'Server', 'data': 'Name taken', 'revertName': data['user'],
+                  'time': time.time()})
+        else:
+            users[data['data']['newUser']] = users[data['user']]
+            session['user'] = data['data']['newUser']
+            emit('chat_response',
+                 {'user': 'Server', 'data': data['user'] + ' is now ' + data['data']['newUser'], 'time': time.time()},
+                 include_self=False, broadcast=True)
+            users.pop(data['user'], None)
+            emit('update_response', {'user': 'Server', 'data': 'Name changed', 'time': time.time()})
+            emit('user_list', {'data': users.keys()}, broadcast=True)
+
+
+@socketio.on('connect', namespace='/chat')
+def connect():
+    emit('history_response', {'history': sorted(history, cmp=lambda x, y: cmp(x['time'], y['time']))})
+
+
+@socketio.on('reconnect', namespace='/chat')
+def reconnect():
+    print('reconnect', request.sid)
+
+
+@socketio.on('disconnect', namespace='/chat')
+def disconnect():
+    users.pop(session['user'], None)
+    new_msg = {'user': 'Server', 'data': session['user'] + ' disconnected!', 'time': time.time()}
+    emit('chat_response', new_msg, broadcast=True, include_self=False)
+    emit('user_list', {'data': users.keys()}, broadcast=True, include_self=False)
+
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=6969, debug=True)
---
+++ static/js/client.js
@@ -0,0 +1,166 @@
+var socket;
+
+function updateUsername() {
+    var username = Cookies.get("username");
+    data = {};
+    if ($("#set_name").val() !== '' && $("#set_name").val() !== username) {
+        data.newUser = $('#set_name').val();
+        Cookies.set("username", $("#set_name").val());
+        showUsername();
+    }
+    var color = Cookies.get("color");
+    if ($("#color").val() !== '#123456' && $('#color').val() !== color) {
+        data.newColor = $("#color").val();
+        Cookies.set("color", $("#color").val());
+    }
+
+    if (data.newUser || data.newColor) {
+        if (socket) {
+            socket.emit('update_user', {
+                data: data,
+                user: username || Cookies.get("username")
+            });
+        }
+        $('#myModal').modal('hide');
+
+        $('#saveUserData').attr('disabled', true);
+    }
+}
+
+function setUsername() {
+    var username = Cookies.get("username");
+    if (username)
+        $("#set_name").val(Cookies.get("username"));
+    var color = Cookies.get("color");
+    if (color) {
+        colorpicker.setColor(color);
+    }
+    $('#myModal').modal('show');
+}
+
+function showUsername() {
+    var username = Cookies.get("username");
+    if (username)
+        $("#username").text(username + ":");
+    else
+        setUsername();
+}
+
+var numMessages = 0;
+var colorpicker;
+
+function connect() {
+    if (!Cookies.get('username'))
+        window.setTimeout(connect, 500);
+    else {
+        socket = io.connect('http://' + document.domain + ':' + location.port + namespace);
+
+        socket.on('history_response', function(data) {
+            for (var message in data.history) {
+                print_message(data.history[message]);
+            }
+            socket.emit('connect_message', {
+                user: Cookies.get("username"),
+                color: Cookies.get('color') || '#000000'
+            });
+        });
+
+        socket.on('chat_response', print_message);
+
+        function print_message(msg) {
+
+            let date = $('<em/>').addClass('text-muted').text(moment.unix(msg.time).format("MM/DD/YY HH:mm:ss "));
+            let message = $('<span/>').text('<' + msg.user + '> ' + msg.data);
+            if (msg.color)
+                message.css('color', msg.color);
+            if (msg.user === 'Server') {
+                message.addClass('text-warning');
+            }
+            if (msg.user === 'Client') {
+                message.addClass('text-danger');
+            }
+            $('#log').append('<br>' + $('<div/>').append(date).append(message).html());
+            $('#log').scrollTop(document.getElementById('log').scrollHeight);
+            if (!window_focus) {
+                numMessages++;
+                window.document.title = "(" + numMessages + ") Best ever chat!";
+            }
+            $.titleAlert('New message!', {
+                requireBlur: true,
+                stopOnFocus: true,
+                interval: 500,
+                duration: 3000
+            });
+        }
+
+        socket.on('user_list', function(msg) {
+            $('#user_list').empty();
+            for (var user in msg['data']) {
+                $('#user_list').append($('<div/>').text(msg['data'][user]));
+            }
+        });
+
+        socket.on('update_response', function(msg) {
+            print_message(msg);
+            if (msg.revertName) {
+                Cookies.set("username", msg.revertName);
+                showUsername();
+            }
+        });
+
+        socket.on('disconnect', function() {
+            if (!reloading) {
+                print_message({user: 'Client', time: moment.now(), data: 'Connection lost!!'});
+            }
+        });
+
+        socket.on('reconnect_attempt', function() {
+            print_message({user: 'Client', time: moment.now(), data: 'Attempting to reconnect to the server...'});
+        })
+    }
+}
+
+var window_focus = true;
+var reloading = false;
+
+window.onbeforeunload = function() {
+    reloading = true;
+    socket.emit('disconnect_request');
+};
+
+$(document).ready(function() {
+    $(window).focus(function() {
+        numMessages = 0;
+        window.document.title = "Best ever chat!";
+        window_focus = true;
+    }).blur(function() {
+        window_focus = false;
+    });
+
+    colorpicker = $.farbtastic("#colorpicker", function(color) {
+        $("#color").val(color);
+        if (Cookies.get("color") !== color)
+            enableSave();
+    });
+
+
+    namespace = '/chat';
+
+    showUsername();
+
+    window.setTimeout(connect, 500);
+
+    $('form#chat').submit(function(event) {
+        socket.emit('broadcast_message', {
+            data: $('#chat_text').val(),
+            user: Cookies.get('username')
+        });
+        $('#chat_text').val('');
+        return false;
+    });
+});
+
+function enableSave() {
+    console.log(event);
+    $('#saveUserData').attr('disabled', false);
+}
