6eb4b8ab3a88d08c56ee7bbcd5a51920eb0d5a60
audrey <audreymccormick@outlook.com>
2017-01-05 21:47:24 -0700

    not using title alert anymore fixes message count issues. so does counting in the right places.

--- static/js/client.js
+++ static/js/client.js
@@ -52,100 +52,6 @@ var numMessages = 0;
 var MAX_RETRIES = 3;
 var colorpicker;
 
-function connect() {
-    if (!Cookies.get('username'))
-        window.setTimeout(connect, 500);
-    else {
-        socket = io.connect('http://' + document.domain + ':' + location.port + namespace, {
-            reconnectionDelay: 1500,
-            reconnectionDelayMax: 4500,
-            reconnectionAttempts: MAX_RETRIES
-        });
-
-        socket.on('session_error', function() {
-            console.log('session error');
-        });
-
-        socket.on('history_response', function(data) {
-            sounds_setting = JSON.parse(Cookies.get('sounds'));
-            Cookies.set('sounds', false);
-            for (var message in data.history) {
-                print_message(data.history[message]);
-            }
-            Cookies.set('sounds', sounds_setting);
-            socket.emit('connect_message', {
-                user: Cookies.get("username"),
-                color: Cookies.get('color') || '#000000'
-            });
-        });
-
-        socket.on('chat_response', print_message);
-
-        socket.on('user_list', function(msg) {
-            $('#user_list').empty();
-            for (var user in msg['data']) {
-                $('#user_list').append($('<div/>').text(msg['data'][user]));
-            }
-        });
-
-        socket.on('update_response', function(msg) {
-            print_message(msg);
-            if (msg.revertName) {
-                Cookies.set("username", msg.revertName);
-                showUsername();
-            }
-        });
-
-        socket.on('disconnect', function() {
-            if (!reloading) {
-                print_message({
-                    user: 'Client',
-                    time: moment().unix(),
-                    data: 'Connection lost!!'
-                });
-            }
-        });
-
-        socket.on('reconnect_attempt', function(number) {
-            print_message({
-                user: 'Client',
-                time: moment().unix(),
-                data: 'Attempting to reconnect to the server... (' + number + '/' + MAX_RETRIES + ')'
-            });
-        });
-
-        socket.on('reconnect_failed', function() {
-            print_message({
-                user: 'Client',
-                time: moment().unix(),
-                data: 'Reconnect failed.'
-            });
-            $('#connectError').modal('show');
-        });
-
-        socket.on('connect_error', function(error) {
-            print_message({
-                user: 'Client',
-                time: moment().unix(),
-                data: 'Connection error'});
-            console.log(error);
-        });
-
-        socket.on('reconnect_error', function(error) {
-            print_message({
-                user: 'Client',
-                time: moment().unix(),
-                data: 'Reonnection error'});
-            console.log(error);
-        });
-    }
-}
-
-function attemptReconnect() {
-    socket.open();
-    $('#connectError').modal('hide');
-}
-
 var window_focus = true;
 var reloading = false;
 
@@ -248,26 +154,10 @@ $(document).ready(function() {
     );
     $('#update-user').validate(updateSettings);
 
-    namespace = '/chat';
-
-
-    window.setTimeout(connect, 500);
-
-
     showUsername();
 
 });
 
-function submitChat(event) {
-    if (event.keyCode === 13) {
-        socket.emit('broadcast_message', {
-            data: $('#chat_text').val(),
-            user: Cookies.get('username')
-        });
-        $('#chat_text').val('');
-        $('#chat_text').focus();
-    }
-}
 
 function print_message(msg) {
     let date = $('<em/>').addClass('text-muted').text(moment.unix(msg.time).format("MM/DD/YY HH:mm:ss "));
@@ -282,9 +172,6 @@ function print_message(msg) {
     }
     $('#log').append('<br>' + $('<div/>').append(date).append(message).html());
     $('#log').scrollTop(document.getElementById('log').scrollHeight);
-    if (!window_focus && numMessages !== 0) {
-        window.document.title = "(" + numMessages + ") Best ever chat!";
-    }
     if (msg.user === 'Server') {
         if (msg.data.includes('disconnected')) play_disconnect();
         else if (msg.data.includes('connected') && !msg.data.includes(Cookies.get('username'))) {
@@ -297,11 +184,8 @@ function print_message(msg) {
     else {
         numMessages++;
         if (msg.user !== 'Client') play_receive();
+        if (!window_focus) {
+            window.document.title = "(" + numMessages + ") Best ever chat!";
+        }
     }
-    $.titleAlert('New message!', {
-        requireBlur: true,
-        stopOnFocus: true,
-        interval: 500,
-        duration: 3000
-    });
 }
