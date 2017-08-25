40dcbf3ee78e647273f26ae29fd32fcf856ff824
audrey <audreymccormick@outlook.com>
2017-08-24 18:32:36 -0600

    Added database to readme and sql file for creating schema/table

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
-#Best Chat Ever
+# Best Chat Ever
 It's, like, really good, oh my gosh.
 
-####To install: 
-pip install -r install/pip-install
-npm install -g less less-plugin-clean-css
-
-####To run:
-lessc ./static/css/chat.less ./static/chat.css --clean-css="--s1 --advanced --compatibility=ie8"
-python tornado_chat.py
+## Setup:
+### Install:
+1. `pip install -r install/pip-install`
 
 
-####Resources:
+2. `npm install -g less less-plugin-clean-css`
+#### Database:
+1. Setup a local MySql database.
+2. Import `install/bestChat.sql` to get the schema and tables.
+3. Make a user 'bestChat' with the password in `tornado_chat.py` and (at least) the following
+permissions to the `bestchat` schema: DELETE, EXECUTE, INSERT, SELECT, SHOW, VIEW, UPDATE
+
+
+## To run:
+1. `lessc ./static/css/chat.less ./static/chat.css --clean-css="--s1 --advanced --compatibility=ie8"`
+
+
+2. `python tornado_chat.py`
+
+
+---
+
+
 favicon from: http://www.iconka.com
 
+
 twemoji: https://github.com/twitter/twemoji
+
 
 emojione: https://www.emojione.com
---
+++ install/bestChat.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE  IF NOT EXISTS `bestchat` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `bestchat`;
+-- MySQL dump 10.13  Distrib 5.6.24, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bestchat
+-- ------------------------------------------------------
+-- Server version	5.6.26-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `parasite`
+--
+
+DROP TABLE IF EXISTS `parasite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `parasite` (
+  `id` varchar(128) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `color` varchar(7) DEFAULT NULL,
+  `sound` tinyint(4) DEFAULT NULL,
+  `username` varchar(32) DEFAULT NULL,
+  `isGuest` bit(1) DEFAULT NULL,
+  `soundSet` varchar(3) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `parasite_id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-08-24 18:28:58
