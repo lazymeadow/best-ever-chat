b339dab96f0dc9e260c7fb11eaa5a500995e3df4
Audrey McCormick <audreymccormick@outlook.com>
2017-09-03 11:24:14 -0600

    moving menu up to header and adding more options, moving form validators to another file

--- static/js/sockjs_client.js
+++ static/js/sockjs_client.js
@@ -12,7 +12,8 @@ $(document).ready(function () {
     // initial hiding of elements
     $('#emoji-list').hide();
     $('#menu').hide();
-    $('#userStats').hide();
+    $('#userSettings').hide();
+    $('#accountSettings').hide();
     $('#connectError').hide();
     $('#imageInput').hide();
     $('#information').hide();
@@ -68,8 +69,6 @@ $(document).ready(function () {
 
     colorPicker = bestColorPicker($('#color'));
     colorPicker.setColor(Cookies.get('color'));
-
-    $('#update-user').validate(updateSettings);
 
     showUsername();
 
@@ -306,7 +305,7 @@ function setUsername() {
     if (username_cookie)
         $("#set_name").val(username_cookie);
 
-    toggleModal('userStats');
+    toggleModal('userSettings');
 }
 
 function showUsername() {
@@ -317,106 +316,6 @@ function showUsername() {
     else
         setUsername();
 }
-
-var updateSettings = {
-    rules: {
-        set_name: {
-            required: true,
-            remote: {
-                url: '/validate_username',
-                type: 'post',
-                data: {
-                    username: function () {
-                        return Cookies.get('username');
-                    },
-                    _xsrf: function () {
-                        return Cookies.get('_xsrf');
-                    }
-                }
-            },
-            minlength: 1,
-            maxlength: 16
-        },
-        color: {
-            required: true
-        },
-        new_password: {
-            required: false,
-            minlength: 3
-        },
-        new_password2: {
-            equalTo: '#new_password'
-        },
-        email: {
-            required: false
-        }
-    },
-    messages: {
-        set_name: {
-            remote: "Invalid name."
-        }
-    },
-    submitHandler: function () {
-        var username = Cookies.get("username");
-        var data = {};
-        var setName = $("#set_name");
-        if (setName.val() !== '' && setName.val() !== username) {
-            data.newUser = setName.val();
-            data.oldUser = Cookies.get('username');
-        }
-        var color = Cookies.get("color");
-        if (colorPicker.val() !== color) {
-            data.newColor = $("#color").val();
-        }
-        var sounds = $('#volume-slider').val();
-        if (sounds !== Cookies.get('sounds')) {
-            data.newSounds = sounds;
-        }
-        var soundSet = $('input[name="sounds-radios"]:checked').val();
-        if (soundSet !== Cookies.get('sound_set')) {
-            data.newSoundSet = soundSet;
-        }
-        var faction = $('input[name="faction"]:checked').val();
-        if (faction !== Cookies.get('faction')) {
-            data.newFaction = faction;
-        }
-        var email = $('#email').val();
-        if (email !== '' && email !== Cookies.get('email')) {
-            data.newEmail = email;
-        }
-
-        var newPassword = $("#new_password");
-        var newPassword2 = $("#new_password2");
-        if (newPassword.val() !== '' && newPassword.val() === newPassword2.val()) {
-            sock.send(JSON.stringify({
-                'type': 'password_change',
-                'data': [newPassword.val(), newPassword.val()],
-                'user': username
-            }));
-            newPassword.val('');
-            newPassword2.val('');
-        }
-
-        if (data.newUser || data.newEmail || data.newFaction || data.newColor || data.newSoundSet || data.newSounds) {
-            if (!sock) connect();
-            sock.send(JSON.stringify({
-                'type': 'userSettings',
-                'settings': data,
-                'user': username
-            }));
-        }
-        else {
-            print_message({
-                user: "Client",
-                message: "No changes made",
-                time: moment().unix()
-            });
-        }
-
-        toggleModal('userStats');
-    }
-};
-
 
 var numMessages = 0;
 
