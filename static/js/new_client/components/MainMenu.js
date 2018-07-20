d5cd62c37284c5b5819fa475bca76bfdb59129f7
Audrey McCormick <mmccormick3@intuit.com>
2018-07-20 15:08:02 -0600

    user settings modal form

--- static/js/new_client/components/MainMenu.js
+++ static/js/new_client/components/MainMenu.js
@@ -22,7 +22,7 @@ class MainMenu extends LoggingClass {
                                 .append($('<div>').addClass('form-element')
                                     .append($('<label>', {text: 'Font Size', for: 'font_size'}))
                                     .append($('<select>', {id: 'font_size'})
-                                        .append($.map([12, 14, 16, 18, 20, 22, 24], (item) => {
+                                        .append($.map([12, 14, 16, 18, 20, 22, 24], item => {
                                             return $('<option>', {value: item, text: item});
                                         }))))
                                 // Hide images by default
@@ -51,7 +51,35 @@ class MainMenu extends LoggingClass {
                     new Modal({
                         form: true,
                         title: 'User Settings',
-                        content: $('<div>'),
+                        content: $('<div>')
+                            .append($('<div>').addClass('form-group')
+                                // Display name
+                                    .append($('<div>').addClass('form-element')
+                                        .append($('<label>', {text: 'Display Name', for: 'username'}))
+                                        .append($('<input>', {id: 'username'})))
+                                    // Volume
+                                    .append($('<div>').addClass('form-element')
+                                        .append($('<label>', {text: 'Volume', for: 'volume'}))
+                                        .append($('<input>', {id: 'volume', type: 'range'}))
+                                        .append($('<div>', {id: 'volume_button'}).append($('<span>').addClass('fa-stack fa').append($('<i>').addClass('fa fa-volume-down fa-stack-2x')))))
+                                    // Sound set
+                                    .append($('<div>').addClass('form-element')
+                                        .append($('<label>', {text: 'Sound Set', for: 'sound_set'}))
+                                        .append($('<select>', {id: 'sound_set'})
+                                            .append($.map(['AIM', 'MSN'], item => {
+                                                return $('<option>', {value: item, text: item});
+                                            }))))
+                                    // Color
+                                    .append($('<div>').addClass('form-element')
+                                        .append($('<label>', {text: 'Color', for: 'color'}))
+                                        .append(bestColorPicker($('<div>', {id: 'color'}))))
+                                    // Faction
+                                    .append($('<div>').addClass('form-element')
+                                        .append($('<label>', {text: 'Faction', for: 'faction'}))
+                                        .append($('<select>', {id: 'faction'})
+                                            .append($('<option>', {value: 'ra', text: 'Rebel Alliance'}))
+                                            .append($('<option>', {value: 'ge', text: 'Galactic Empire'}))))
+                            ),
                         buttonText: 'Save',
                         buttonClickHandler: () => {
                             this.debug('User settings saved');
