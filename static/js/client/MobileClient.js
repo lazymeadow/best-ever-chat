a2e0bd88deaf17a50721b85eceb1d73c1a46d084
audrey <audreymccormick@outlook.com>
2018-11-18 09:55:37 -0700

    parse emojis on room change for mobile

--- static/js/client/MobileClient.js
+++ static/js/client/MobileClient.js
@@ -3,6 +3,7 @@ import {BestEvarChatClient} from "./BestEvarChatClient";
 import {RoomManager} from "../rooms";
 import {MainMenu} from "../components";
 import {Settings} from "../util";
+import {_parseEmojis} from "../lib";
 
 export class MobileClient extends BestEvarChatClient {
     constructor() {
@@ -19,6 +20,8 @@ export class MobileClient extends BestEvarChatClient {
         $('#current-log-name').text(Settings.activeLogType === 'thread'
             ? this._userManager.getActiveThreadName()
             : this._roomManager.getActiveRoomName());
+
+        _parseEmojis();
 
         return 'Best Evar Chat 3.0';
     }
