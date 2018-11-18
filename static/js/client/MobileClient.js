2381cee34c43f3d16cba5b6aa515b46a8c9a6f54
audrey <audreymccormick@outlook.com>
2018-11-17 19:54:07 -0700

    add current log name to mobile header

--- static/js/client/MobileClient.js
+++ static/js/client/MobileClient.js
@@ -1,6 +1,8 @@
+import $ from 'jquery';
 import {BestEvarChatClient} from "./BestEvarChatClient";
 import {RoomManager} from "../rooms";
 import {MainMenu} from "../components";
+import {Settings} from "../util";
 
 export class MobileClient extends BestEvarChatClient {
     constructor() {
@@ -11,5 +13,13 @@ export class MobileClient extends BestEvarChatClient {
             about: true
         });
         this._roomManager = new RoomManager(this, this._messageLog, this._soundManager);
+    }
+
+    _getTitle() {
+        $('#current-log-name').text(Settings.activeLogType === 'thread'
+            ? this._userManager.getActiveThreadName()
+            : this._roomManager.getActiveRoomName());
+
+        return 'Best Evar Chat 3.0';
     }
 }
