070828e33ba43058389a79c26282af4157c82de1
Aaron Russell <russell.aaron94@gmail.com>
2021-12-17 17:07:21 -0700

    Start compose migration by refactoring the login activity

--- android-client/app/src/main/java/com/bestevarchat/login/LogInActivity.kt
+++ android-client/app/src/main/java/com/bestevarchat/login/LogInActivity.kt
@@ -1,56 +1,91 @@
 package com.bestevarchat.login
 
-import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import com.bestevarchat.AuthService
 import com.bestevarchat.chat.ChatActivity
-import com.bestevarchat.R
+import com.bestevarchat.ui.theme.BestEvarChatTheme
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-class LogInActivity : AppCompatActivity() {
+class LogInActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_log_in)
-
-		// If the user is already authenticated, bypass LogInActivity
-		if (AuthService.isAuthenticated(this)) {
-			goToChat()
-		}
-
-		val viewModel = ViewModelProvider(this).get(LogInViewModel::class.java)
-
-		viewModel.authResponse.observe(this, {
-			Log.d("LogInActivity", "Response: {success: ${it.success}, message: ${it.message}}")
-
-			if (it.success) {
-				goToChat()
-			} else {
-				AlertDialog.Builder(this)
-					.setTitle("Oh no!")
-					.setMessage("Something went wrong!")
-					.setPositiveButton("OK") { dialog, _ ->
-						dialog.cancel()
-					}
-					.create()
-					.show()
+		setContent {
+			BestEvarChatTheme {
+				// A surface container using the 'background' color from the theme
+				Surface(color = MaterialTheme.colors.background) {
+					LogIn()
+				}
 			}
-		})
-
-		findViewById<Button>(R.id.buttonLogIn).setOnClickListener {
-			viewModel.authenticate(
-				findViewById<EditText>(R.id.editUsername).text.toString(),
-				findViewById<EditText>(R.id.editPassword).text.toString()
-			)
 		}
 	}
+}
 
-	private fun goToChat() {
-		startActivity(Intent(this, ChatActivity::class.java))
-		finish()
+@Composable
+fun LogIn() {
+	Column(
+		modifier = Modifier
+			.fillMaxHeight()
+			.fillMaxWidth(),
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally,
+	) {
+		var username by remember { mutableStateOf(TextFieldValue("")) }
+		TextField(value = username, onValueChange = { new -> username = new }, label = { Text("Username") })
+
+		Spacer(modifier = Modifier.height(Dp(20F)))
+
+		var password by remember { mutableStateOf(TextFieldValue("")) }
+		TextField(
+			value = password,
+			onValueChange = { new -> password = new },
+			label = { Text("Password") },
+			visualTransformation = PasswordVisualTransformation(),
+			keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+		)
+
+		Spacer(modifier = Modifier.height(Dp(20F)))
+
+		val context = LocalContext.current
+		Button(
+			onClick = {
+				AuthService.authenticate(context, username.text, password.text) {
+					if (it.success) {
+						context.startActivity(Intent(context, ChatActivity::class.java))
+					} else {
+						MaterialAlertDialogBuilder(context)
+							.setTitle("Oh no!")
+							.setMessage("Something went wrong!")
+							.setPositiveButton("Okay") { dialog, _ -> dialog.cancel() }
+							.create()
+							.show()
+					}
+				}
+			}
+		) {
+			Text(text = "Log in")
+		}
+	}
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+	BestEvarChatTheme {
+		LogIn()
 	}
 }
